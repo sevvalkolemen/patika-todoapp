7f983ee57365524b7737e421a6ca12ac821dee8a
sevvalkolemen <seval.kolemen@gmail.com>
2022-07-26 16:12:58 +0300

    added data on Context

--- src/components/Header/NewTodoForm/index.js
+++ src/components/Header/NewTodoForm/index.js
@@ -1,13 +1,18 @@
 import React from "react";
 import { Formik, Form, Field, useFormik } from "formik";
 import validationSchema from "./validations";
+import { useTodo } from "../../../contexts/TodoContext";
+
 
 function NewTodoForm() {
+  const { addTodo } = useTodo();
+
   return (
     <Formik
-      initialValues={{ text:"" }}
+      initialValues={{ text: "" }}
       onSubmit={(values, bag) => {
         console.log(values);
+        addTodo(values.text);
         bag.resetForm();
       }}
       validationSchema={validationSchema}
--- src/contexts/TodoContext.js
+++ src/contexts/TodoContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const TodoContext = createContext();
 
@@ -11,9 +12,12 @@ export const TodoProvider = ({ children }) => {
     }
   ]);
 
+  const addTodo = (text) => setTodos((prev) => [...prev,{id: uuidv4(), completed: false, text}])
+
   const values = {
     todos,
     setTodos,
+    addTodo,
   };
 
   return <TodoContext.Provider value={values}>{children}</TodoContext.Provider>;
